28934fd89b8db2df0f5ed699f66a667fe981656d
slandelle <slandelle@gatling.io>
2026-01-05 08:41:10 +0100

    chore: update copyright year

--- content/guides/optimize-scripts/writing-realistic-tests/code/AdvancedTutorialSampleKotlin.kt
+++ content/guides/optimize-scripts/writing-realistic-tests/code/AdvancedTutorialSampleKotlin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2025 GatlingCorp (https://gatling.io)
+ * Copyright 2011-2026 GatlingCorp (https://gatling.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- import io.gatling.javaapi.core.*
+import io.gatling.javaapi.core.*
  import io.gatling.javaapi.core.CoreDsl.*
  import io.gatling.javaapi.http.HttpDsl.*
  import io.gatling.javaapi.http.*
