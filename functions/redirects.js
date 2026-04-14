cececa45f3a7cf892b87538f475105ce23c6b9e5
Guillaume Corré <gui.corre@gmail.com>
2026-04-14 11:04:40 +0200

    chore: update functions to latest live version

--- functions/redirects.js
+++ functions/redirects.js
@@ -3,7 +3,7 @@ import cf from "cloudfront";
 const kvsId = "kvsId"
 const kvsHandle = cf.kvs(kvsId);
 
-const extensions = [".css", ".html", ".ico", ".js", ".json", ".png", ".svg", ".ttf", ".txt", ".xml", ".webmanifest", ".woff", ".woff2"];
+const extensions = [".css", ".gif", ".html", ".ico", ".js", ".json", ".md", ".png", ".svg", ".ttf", ".txt", ".webmanifest", ".webp", ".woff", ".woff2", ".xml"];
 
 function hasKnownFileExtension(uri) {
   for (const index in extensions) {
